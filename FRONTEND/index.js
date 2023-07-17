d2310eaf735f37056064d6a6fb92a58b79f93ff8
Albert-Debbarma <propertiesofalbert.com>
2023-07-18 01:33:25 +0530

    url commit

--- FRONTEND/index.js
+++ FRONTEND/index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://http://localhost:3000";
+const baseURL = "https://shera-filesharing.onrender.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
